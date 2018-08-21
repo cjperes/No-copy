56acf8e672546d79cfb9b837bab2908be3741a1b
cjperes <caio@bbcreative.org>
2018-08-21 12:53:00 -0300

    start

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # No copy Wordpress Plugin
+
+<p>Simple plugin for turning off copies on a website</p>
+
+<h1>Light and simple</h1>
+This plugin uses javascript to disable the copy, if the user disables the browser javascript, this plugin will not work!
+
+Wordpress plugin link: https://wordpress.org/plugins/easy-anti-spam-bots/
+
+
+Works perfectly with all themes and editors of Wordpress
+
+
+The software is licensed to you "as is" and without any warranty. If you do not agree to the license, do not use the software.
---
+++ no-copy.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: No copy
+ * Plugin URI: https://github.com/cjperes/no-copy
+ * Description: Simple plugin for turning off copies on a website
+ * Author: Caio Peres
+ * Author URI: https://github.com/cjperes
+ * Version: 1.0.0
+ */
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // tela branca caso o plugin for acessado direto
+}
+
+// add function in footer
+add_action( 'wp_footer', 'my_footer_scripts' );
+function my_footer_scripts(){
+  ?>
+  <script language="javascript">
+    function clearData(){
+        window.clipboardData.setData('text','') 
+    }
+    function cldata(){
+        if(clipboardData){
+            clipboardData.clearData();
+        }
+    }
+    setInterval("cldata();", 1000);
+</script>
+
+
+<body ondragstart="return false;" onselectstart="return false;"  oncontextmenu="return false;" onload="clearData();" onblur="clearData();">
+  <?php
+}
+
